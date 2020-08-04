39a087053d7f437eb83d64833424f41eebf016e9
shaneneiman <shane.neiman@gmail.com>
2020-08-04 15:02:56 -0400

    css bug fixed, JS added, and html edited

--- main.js
+++ main.js
@@ -4,17 +4,75 @@ function getContext () {
     return canvas.getContext("2d")
 }
 
-function makeObject (x,y, velocity, size, image) {
-       let object = {     x: x,       y: y,    
-         image: image,        velocity: velocity.      
-          size: size,      image: image;   }   
-          return object}
+// Global Variables
+
+const WIDTH = 1000
+const HEIGHT = 1000
+const rabbitColor = rgb(255 , 255, 255)
+const wolfColor = rgb(220, 2220, 220)
+const bearColor = rgb(165, 42, 42)
+let animationId
+
+// Start Function
+function startGame () {
+  let menuBox = document.querySelector("#menu-box")
+  menuBox.classList.add(".hidden")
+  animationId = window.setInterval(drawScreen, 10)
 
 
-function rabbit (x,y, width, height, )
-function rabbit (x,y, width, height)
-function rabbit (x,y, width, height)
-function rabbit (x,y, width, height)
-function rabbit (x,y, width, height)
+//Function to Clear Screen
+function clearScreen () {
+  let ctx = getContext ()
+  ctx.clearRect(0,0, WIDTH, HEIGHT)
+}
 
+// Function to prodcue chararacter objects
+function makeCharacter (x, y, width, height, color) {
+  let character = {
+    x: x,
+    y: y,
+    width: width,
+    height: height,
+    color: color,
+  }
+  return character
+}
 
+// Creation of character objects 
+let rabbit = makeCharacter(450, 900, 100, 100, rabbitColor)
+let wolf = makeCharacter(300, 800, 125, 100, wolfColor)
+let bear = makeCharacter(600, 700, 200, 100, bearColor)
+
+//Function to draw characters
+function drawCharacter (x, y, width, height, color) {
+  let ctx = getContext ()
+  ctx.fillStyle = color
+  ctx.fillRec(x, y, width, height)
+}
+// Function to draw objects on the screen 
+function drawScreen() {
+  clearScreen()
+  drawCharacter(rabbit.x, rabbit.y, rabbit.width, rabbit.height, rabbit.color)
+  drawCharacter(wolf.x, wolf.y, wolf.width, wolf.height, wolf.color)
+  drawCharacter(bear.x, bear.y, bear.width, bear.height, bear.color)
+}
+
+//Event handler to control movement of the rabbit via arrow keys
+function keyPressListener (event) {
+  if (event.key === "ArrowUp") {
+      rabbit.y -= 100
+  } else if (event.key === "ArrowDown") {
+      rabbit.y += 100
+  } else if (event.key === "ArrowLeft") {
+      rabbit.x -= 10
+  } else if (event.key === "ArrowRight") {
+      rabbit.x += 10
+  }
+}
+
+//Event Listner to hide menu box and start game
+let startButton = document.querySelector("#button")
+startButton.addEventListener("click", startGame)
+
+//Event Listener for event handle keypress listener
+window.addEventListener("keydown", keyPressListener)
