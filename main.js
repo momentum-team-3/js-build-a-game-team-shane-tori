c55f50df7c4d452c23babf35ff971b22a732d181
Tori Helpingstine <torihelpingstine@gmail.com>
2020-08-03 20:13:40 -0400

    HopperGame

---
+++ main.js
@@ -0,0 +1,20 @@
+let canvas = document.querySelector ("#canvas")
+
+function getContext () {
+    return canvas.getContext("2d")
+}
+
+function makeObject (x,y, velocity, size, image) {
+       let object = {     x: x,       y: y,    
+         image: image,        velocity: velocity.      
+          size: size,      image: image;   }   
+          return object}
+
+
+function rabbit (x,y, width, height, )
+function rabbit (x,y, width, height)
+function rabbit (x,y, width, height)
+function rabbit (x,y, width, height)
+function rabbit (x,y, width, height)
+
+
