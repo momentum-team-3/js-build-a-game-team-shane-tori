0d116a34fbe08c4ed742d8a2a64ca326b6e2677e
shaneneiman <shane.neiman@gmail.com>
2020-08-05 23:14:24 -0400

    cleaned up old JS code and added loop function to draw characters

--- main.js
+++ main.js
@@ -7,42 +7,16 @@ function getContext () {
 // Global Variables
 const WIDTH = canvas.width
 const HEIGHT = canvas.height
-/* 
-const rabbitColor = "rgb(255 , 255, 255)"
-const wolfColor = "rgb(220, 2220, 220)"
-const bearColor = "rgb(165, 42, 42)"
-const boarColor = "rgb(120, 50, 50)"
-const cayoteColor = "rgb(130, 20, 60)"
-const lionColor = "rgb(90, 20, 40)"
-const foxColor = "rgb(200, 95, 50)"
-const catColor = "rgb(110, 80, 30)"
-*/
-const wolfImage = new Image()
-wolfImage.src= "AnimalsFarmAndPuzzlePack/Characters/Fox_Right.png"
 
-const bearImage = new Image()
-bearImage.src = "AnimalsFarmAndPuzzlePack/Characters/Cat_Right.png"
-
-const boarImage = new Image()
-boarImage.src = "AnimalsFarmAndPuzzlePack/Characters/Fox_Right.png"
-
-const cayoteImage = new Image()
-cayoteImage.src = "AnimalsFarmAndPuzzlePack/Characters/Cat_Right.png"
-
-const lionImage = new Image()
-lionImage.src= "AnimalsFarmAndPuzzlePack/Characters/Fox_Right.png"
-
-const foxImage = new Image()
-foxImage.src = "AnimalsFarmAndPuzzlePack/Characters/Cat_Right.png"
-
-const catImage = new Image()
-catImage.src = "AnimalsFarmAndPuzzlePack/Characters/Fox_Right.png"
-
+//Image Variables
 const rabbitImage = new Image()
 rabbitImage.src = "AnimalsFarmAndPuzzlePack/Characters/Rabbit_Up.png"
 
-const foxyImage = new Image()
-foxyImage.src = "AnimalsFarmAndPuzzlePack/Characters/Cat_Right.png"
+const foxImage = new Image()
+foxImage.src = "AnimalsFarmAndPuzzlePack/Characters/Fox_Right.png"
+
+const catImage = new Image()
+catImage.src = "AnimalsFarmAndPuzzlePack/Characters/Cat_Right.png"
 
 const rabbitDeadimg= new Image ()
 rabbitDeadimg.src = "AnimalsFarmAndPuzzlePack/Characters/Rabbit_Dead.png"
@@ -51,9 +25,6 @@ const carrotImage= new Image ()
 carrotImage.src = "AnimalsFarmAndPuzzlePack/Characters/Carrot.png"
 
 let animationId
-/*
-enemyImages = [new Image(), new Image(), new Image(), new Image()]
-*/
 
 // Start Function
 function startGame () {
@@ -76,29 +47,6 @@ function clearScreen () {
   ctx.clearRect(0,0, WIDTH, HEIGHT)
 }
 
-/*
-//function to creat spawn points
-function createSpawnPoint (x,y) {
-  return {
-    x: x,
-    y: y,
-    velocity: velocity,
-    vx: () => spawn.velocity[0],
-    vy: () => spawn.velocity[1],
-  }
-}
-
-//Spawn Points
-let spawn1 = createSpawnPoint(0, 640, [1,0])
-let spawn2 = createSpawnPoint(0, 560, [1.5,0])
-let spawn3 = createSpawnPoint(0, 480, [2,0])
-let spawn4 = createSpawnPoint(0, 400, [2.5,0])
-let spawn5 = createSpawnPoint(0, 320, [3,0])
-let spawn6 = createSpawnPoint(0, 240, [3.5,0])
-let spawn7 = createSpawnPoint(0, 160, [4,0])
-let spawn8 = createSpawnPoint(0, 80, [5,0])
-*/
-
 // Function to prodcue chararacter objects
 function makeCharacter (x,y, width, height, image) {
   return {
@@ -116,48 +64,43 @@ function makeCharacter (x,y, width, height, image) {
 
 // Creation of character objects 
 let rabbit = makeCharacter(360, 720, 80, 80, rabbitImage)
-let wolf = makeCharacter(300, 640, 125, 80, wolfImage)
+let wolf = makeCharacter(300, 640, 80, 80, catImage)
 wolf.velocity = [3.5,0]
-let bear = makeCharacter(200, 560, 160, 80, bearImage)
+let bear = makeCharacter(200, 560, 80, 80, foxImage)
 bear.velocity = [1,0]
-let boar = makeCharacter(200, 480, 150, 80, boarImage)
+let boar = makeCharacter(200, 480, 80, 80, catImage)
 boar.velocity= [3,0]
-let cayote = makeCharacter(200, 400, 130, 80, cayoteImage)
+let cayote = makeCharacter(200, 400, 80, 80, foxImage)
 cayote.velocity= [3.5,0]
-let lion = makeCharacter(200, 320, 140, 80, lionImage)
+let lion = makeCharacter(200, 320, 80, 80, catImage)
 lion.velocity= [1.5,0]
-let fox = makeCharacter(200, 240, 140, 80, foxImage)
+let fox = makeCharacter(200, 240, 80, 80, foxImage)
 fox.velocity= [4,0]
-let cat = makeCharacter(200, 160, 110, 80, catImage)
+let cat = makeCharacter(200, 160, 80, 80, catImage)
 cat.velocity= [3.5,0]
-let foxy = makeCharacter(200, 80, 100, 80, foxyImage)
+let foxy = makeCharacter(200, 80, 80, 80, foxImage)
 foxy.velocity= [2.5,0]
 
-//victory area
-let carrot = makeCharacter(200, 50, 90, 80, carrotImage)
-carrot.velocity= [.1,0]
+// Array of characters to be drawn
+const characters = [rabbit, wolf, bear, boar, cayote, lion, fox, cat, foxy]
 
-//Array of Enemies
+// Array of Enemies
 const enemies = [wolf, bear, boar, cayote, lion, fox, cat, foxy]
 
 //Function to draw characters
-function drawCharacter (x, y, width, height, image) {
+function drawCharacter () {
   let ctx = getContext ()
-  ctx.drawImage(image, x, y, width, height)
- 
+  for (let char of characters) {
+    ctx.drawImage(char.image, char.x, char.y, char.width, char.height)
+  }
 }
-/* reset game function */
 
 // Function to draw objects on the screen 
 function drawScreen() {
     let playerDead= false
   clearScreen()
   updateEnemyPosition()
-  if (collision()){
-      playerDead= true
-    window.clearInterval(animationId)
-    alert(("You got eaten!"))
-  }
+  checkforCollisions()
   
 if (playerDead) { 
     drawCharacter(rabbit.x,rabbit.y, rabbit.width, rabbit.height, rabbitDeadimg)
@@ -166,20 +109,8 @@ if (playerDead) {
 
 }
 endGame()
-  
-  drawCharacter(wolf.x, wolf.y, wolf.width, wolf.height, wolf.image)
-  drawCharacter(bear.x, bear.y, bear.width, bear.height, bear.image)
-  drawCharacter(boar.x, boar.y, boar.width, boar.height, boar.image)
-  drawCharacter(cayote.x, cayote.y, cayote.width, cayote.height, cayote.image)
-  drawCharacter(lion.x, lion.y, lion.width, lion.height, lion.image)
-  drawCharacter(fox.x, fox.y, fox.width, fox.height, fox.image)
-  drawCharacter(cat.x, cat.y, cat.width, cat.height, cat.image)
-  drawCharacter(foxy.x, foxy.y, foxy.width, foxy.height, foxy.image)
-
-  drawCharacter(carrot.x, carrot.y, carrot.width, carrot.height, carrot.image)
+drawCharacter()
 }
-  
-
 //Event handler to control movement of the rabbit via arrow keys
 function keyPressListener (event) {
   if (event.key === "ArrowUp") {
@@ -205,18 +136,26 @@ function keyPressListener (event) {
     }
 }
 
-function collision () {
-  for (let enemy of enemies) {
-  if (rabbit.x < enemy.x + enemy.width &&
-    rabbit.x + rabbit.width > enemy.x &&
-    rabbit.y < enemy.y + enemy.height &&
-    rabbit.y + rabbit.height > enemy.y) {
-        return true
-      } else {
+
+function detectCollision(o1, o2) {
+  if (o1.x < o2.x + o2.width &&
+    o1.x + o1.width > o2.x &&
+    o1.y < o2.y + o2.height &&
+    o1.y + o1.height > o2.y) {
+      return true
+    } else {
         return false
       }
+}
+
+function checkforCollisions () {
+  for (let enemy of enemies) {
+    if (detectCollision(rabbit, enemy)) {
+      window.clearInterval(animationId)
+    alert(("You got eaten!"))
+    }
   }
- }
+}
 
 // Update Enemy Position
 function updateEnemyPosition () {
@@ -232,36 +171,6 @@ function updateEnemyPosition () {
     }
 }
 }
-/*
-function updateWolfPosition () {
-  wolf.x += wolf.velocity[0]
-  wolf.y += wolf.velocity[1]
-
-  if (wolf.x >= (wolf.xUpper + 40)) {
-    wolf.x = wolf.xLower
-    }
-  if (wolf.x <= (wolf.xLower - 40)) {
-    wolf.x = (wolf.xUpper + 40)
-    }
-}
-
-function updateBearPosition () {
-  bear.x += bear.velocity[0]
-  bear.y += bear.velocity[1]
-
-  if (bear.x >= (bear.xUpper + 40)) {
-    bear.x = bear.xLower
-    }
-  if (bear.x <= (bear.xLower - 40)) {
-    bear.x = (bear.xUpper + 40)
-    }
-}
-*/
-//Event Listner to hide menu box and start game
-//
-
-// let startButton = document.querySelector("#start-button")
-// startButton.addEventListener("click", startGame)
 
 //Event Listener for event handle keypress listener
 window.addEventListener("keydown", keyPressListener)
